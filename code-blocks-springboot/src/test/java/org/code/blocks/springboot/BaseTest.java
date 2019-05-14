4aa5c2493330b71872dca332ffa16d6b90334b09
darwindu <252921602@qq.com>
2019-05-14 20:22:24 +0800

    gradle complie upgrade implementation

--- code-blocks-springboot/src/test/java/org/code/blocks/springboot/BaseTest.java
+++ code-blocks-springboot/src/test/java/org/code/blocks/springboot/BaseTest.java
@@ -2,8 +2,6 @@ package org.code.blocks.springboot;
 
 import java.util.concurrent.TimeUnit;
 
-import org.code.blocks.springboot.service.HelloWorldSpringBootService;
-import org.code.blocks.springboot.service.impl.HelloWorldSpringBootServiceImpl;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -11,10 +9,7 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
